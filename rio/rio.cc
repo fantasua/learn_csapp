55bb390b6e403de9b0bc43b07692a5dc8521440f
steveyan <steveyan@tencent.com>
2021-01-01 18:20:12 +0800

    working on rio_read function

--- rio/rio.cc
+++ rio/rio.cc
@@ -1,21 +1,43 @@
 #include "rio.h"
+
+#include <stdlib.h>
+
 #include "errno.h"
+
+
+ssize_t rio_read(int fd, char *buf, size_t n) {
+
+
+}
+
+rio_buf_type *rio_buffer_init(int fd, size_t buf_size){
+  rio_buf_type *rio_buf = (rio_buf_type*)malloc(sizeof(rio_buf_type));
+  rio_buf->rio_fd = fd;
+  rio_buf->rio_buffer = (char*)malloc(sizeof(char)*buf_size);
+  rio_buf->rio_buffer_ptr = rio_buf->rio_buffer;
+  rio_buf->rio_bytes_left = buf_size;
+  return rio_buf;
+}
+
+void rio_buffer_release(rio_buf_type *rio_buffer) {
+  rio_buffer->rio_buffer_ptr = NULL;
+  free(rio_buffer->rio_buffer);
+}
+
 ssize_t rio_readn(int fd, void *buffer, size_t n) {
   size_t bytes_left = n;
   ssize_t bytes_read;
-  char* buf = buffer;
+  char *buf = buffer;
 
   // 从fd中读取内容到buffer中
   while (bytes_left > 0) {
-    if((bytes_read = read(fd, buf, bytes_left)) < 0) {
-      if(errno == EINTR) {
+    if ((bytes_read = read(fd, buf, bytes_left)) < 0) {
+      if (errno == EINTR) {
         bytes_read = 0;
-      }
-      else {
+      } else {
         return -1;
       }
-    }
-    else if(bytes_read == 0){
+    } else if (bytes_read == 0) {
       break;
     }
     bytes_left -= bytes_read;
--- rio/rio.h
+++ rio/rio.h
@@ -1,11 +1,23 @@
 #include <unistd.h>
 
+#include "rio_type.h"
+
+#pragma once
+
 // robust io functions
 
 // read from fd with user space buffer
+ssize_t rio_read(int fd, char *buf, size_t n);
 
 
+// init && release the rio_buffer
+// init the rio_buffer
+rio_buf_type *rio_buffer_init(int fd, size_t buf_size = RIO_BUF_SIZE);
+// release the rio_buffer
+void rio_buffer_release(rio_buf_type *rio_buffer);
 
+
+// rio functions with no user space buffer
 // read n bytes from fd with no user space buffer
 ssize_t rio_readn(int fd, void *buffer, size_t n);
 
